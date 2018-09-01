3c2907cb82efe93a89329b9fde6b7a82e223dc30
Hodaka hashiba <b1015253@fun.ac.jp>
2018-09-01 15:12:47 +0900

    Material UI っぽいロードアイコンの実装

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CircleLoader from './CircleLoader.js';
 import CircleLoaderAnother from './CircleLoaderAnother.js';
 import CircleGradient from './CircleGradient.js';
 import CircleWaves from './CircleWaves.js';
-
+import CircleLoaderMarcky from './CircleLoaderMarcky.js';
 
 export default class App extends Component {
   render() {
@@ -12,6 +12,7 @@ export default class App extends Component {
       <div className="App">
         <CircleLoader />
         <CircleLoaderAnother />
+        <CircleLoaderMarcky />
         <CircleGradient />
         <CircleWaves />
       </div>
---
+++ src/CircleLoaderMarcky.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './CircleLoaderMarcky.css';
+
+export default class CircleLoaderMarcky extends Component {
+  render() {
+    return (
+      <div className="CircleLoaderMarcky">
+
+      </div>
+    );
+  }
+}
